7a063cd2b024850d898d3576488e9ee8b269b822
Daniel Ntege <danientege785@gmail.com>
2025-04-22 20:49:27 +0300

    feat: Update email service authentication variable names for consistency

--- utils/emailService.js
+++ utils/emailService.js
@@ -8,8 +8,8 @@ export const sendEmail = async (options) => {
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
       auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     })
 
