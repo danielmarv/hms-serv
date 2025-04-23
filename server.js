18d6d9879782b2fdf6401016bedaf710aa0ed407
Daniel Ntege <danientege785@gmail.com>
2025-04-23 21:00:08 +0300

    refactor: Remove redundant MongoDB connection comments for cleaner code

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ process.on("uncaughtException", (err) => {
   process.exit(1)
 })
 
-// Connect to MongoDB
-// Connect MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
