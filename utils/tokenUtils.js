c5d866f55124a980e519367b16a46783ad2632c0
Daniel Ntege <danientege785@gmail.com>
2025-04-23 21:01:58 +0300

    refactor: Remove commented-out generation of refresh token for cleaner code

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -9,7 +9,6 @@ export const generateAccessToken = (userId) => {
   })
 }
 
-// Generate refresh token
 export const generateRefreshToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_REFRESH_SECRET || process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_REFRESH_EXPIRY || "7d",
