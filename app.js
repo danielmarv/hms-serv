f71b6a4eaf4977541a0588762bb2cbbd24839233
Daniel Ntege <danientege785@gmail.com>
2025-04-23 20:56:51 +0300

    refactor: Clean up CORS middleware usage for improved clarity

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ dotenv.config()
 const app = express()
 
 app.use(helmet())
-app.use(cors()) // Enable CORS
+app.use(cors())
 app.use(express.json({ limit: "10kb" })) // Body parser, reading data from body into req.body
 app.use(express.urlencoded({ extended: true, limit: "10kb" }))
 
