900820328c0e1079c1df57122ccef0fdcfbd2927
Daniel Ntege <danientege785@gmail.com>
2025-04-23 21:01:39 +0300

    refactor: Remove commented-out access token generation for cleaner code

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -3,7 +3,6 @@ import crypto from "crypto"
 
 const tokenBlacklist = new Set()
 
-// Generate access token
 export const generateAccessToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_ACCESS_EXPIRY || "7d",
